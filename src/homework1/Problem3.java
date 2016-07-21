a999b1513c2610084c67a31a12e8eb147c1a6188
Brandon Lenz <Brandon Lenz>
2016-07-20 17:32:49 -0700

    hw 1_3

---
+++ src/homework1/Problem3.java
@@ -0,0 +1,24 @@
+package homework1;
+
+import java.util.Scanner;
+
+public class Problem3 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int x;
+		int y;
+		int sum;
+		System.out.print("Enter First Integer:");
+		x = input.nextInt();
+		System.out.print("Enter Second Integer:");
+		y = input.nextInt();
+		sum = x + y;
+		System.out.printf("Sum is %d\n", sum);
+		
+		
+		
+	}
+
+}
